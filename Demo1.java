2a12eea8b027e47cb13d1ad394af0e465265dbc3
Divya19052001 <vdivyashree1@gmail.com>
2023-02-06 18:49:01 +0530

    pushing to ur repo

---
+++ Demo1.java
@@ -0,0 +1,13 @@
+class Demo1
+{
+  public static void main (String[ ] args)	   
+  {
+  System.out.println(20+20);
+  System.out.println("The value is"+20);
+  System.out.println("The value is"+20+20);
+  System.out.println(20+20+"is the value");
+  System.out.println("The value is "+(20+20) );
+  System.out.println(" The value is "+(1/2));
+  System.out.println("The value is "+(1/2.0));
+  }
+}
